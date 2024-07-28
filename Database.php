bcd5c0b0e7ef53ab2401570f26ae1bb9a9357326
Gurneetkaur01 <gurneetk715@gmail.com>
2024-07-28 07:30:04 -0400

    assignment completed

--- Database.php
+++ Database.php
@@ -3,7 +3,7 @@ class Database {
     private $servername = "localhost";
     private $username = "root";
     private $password = "root";
-    private $dbname = "gurneetDB";
+    private $dbname = "gurneetdb";
     public $conn;
 
     public function __construct() {
